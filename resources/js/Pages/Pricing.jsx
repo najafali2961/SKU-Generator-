5c99d0d249529fa6c75a4c702a7bb0683cd98227
najjaf <najaf2961@gmail.com>
2026-02-19 12:35:14 +0500

    fixed

--- resources/js/Pages/Pricing.jsx
+++ resources/js/Pages/Pricing.jsx
@@ -240,8 +240,10 @@ export default function Pricing({
                                         <div
                                             className={`${
                                                 isPopular
-                                                    ? "border-2 border-green-600 shadow-lg shadow-green-500/25 rounded-2xl"
-                                                    : ""
+                                                    ? "border-2 border-yellow-200 shadow-lg shadow-yellow-500/25 rounded-2xl"
+                                                    : isCurrent
+                                                      ? "border-2 border-green-600 shadow-lg shadow-green-500/25 rounded-2xl"
+                                                      : ""
                                             }`}
                                         >
                                             <Card>
